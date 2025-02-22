ce21c06a8a02195cd754cfc4897764fd4ee14bd3
KeEbEe123 <keertan.k@gmail.com>
2025-02-22 11:08:04 +0530

    changed app name

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">Aora!</Text>
+      <Text className="text-3xl font-pblack">HosteLite</Text>
       <StatusBar style="auto" />
       <Link href="./home" className="text-blue-400">
         go to home
